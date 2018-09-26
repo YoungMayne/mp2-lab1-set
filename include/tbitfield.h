8ae4964bea0e37d3822f10c72ea23b7ee9d24ba6
Ilya <ostrovsky1699@gmail.com>
2018-09-26 19:09:23 +0300

    Готово

--- include/tbitfield.h
+++ include/tbitfield.h
@@ -8,6 +8,8 @@
 #ifndef __BITFIELD_H__
 #define __BITFIELD_H__
 
+#define SIZE (sizeof(TELEM) * 8)
+
 #include <iostream>
 
 using namespace std;
@@ -20,7 +22,6 @@ private:
   int  BitLen; // длина битового поля - макс. к-во битов
   TELEM *pMem; // память для представления битового поля
   int  MemLen; // к-во эл-тов Мем для представления бит.поля
-
   // методы реализации
   int   GetMemIndex(const int n) const; // индекс в pМем для бита n       (#О2)
   TELEM GetMemMask (const int n) const; // битовая маска для бита n       (#О3)
@@ -30,14 +31,14 @@ public:
   ~TBitField();                      //                                    (#С)
 
   // доступ к битам
+  int  GetBit(const int n) const; // получить значение бита               (#Л1)
   int GetLength(void) const;      // получить длину (к-во битов)           (#О)
   void SetBit(const int n);       // установить бит                       (#О4)
   void ClrBit(const int n);       // очистить бит                         (#П2)
-  int  GetBit(const int n) const; // получить значение бита               (#Л1)
 
   // битовые операции
-  int operator==(const TBitField &bf) const; // сравнение                 (#О5)
-  int operator!=(const TBitField &bf) const; // сравнение
+  bool operator==(const TBitField &bf) const; // сравнение                 (#О5)
+  bool operator!=(const TBitField &bf) const; // сравнение
   TBitField& operator=(const TBitField &bf); // присваивание              (#П3)
   TBitField  operator|(const TBitField &bf); // операция "или"            (#О6)
   TBitField  operator&(const TBitField &bf); // операция "и"              (#Л2)
